1b2ec87c55c2eb86f74598b21bc260e1dfd2655d
Remuladgryta <remuladgryta@gmail.com>
2017-09-08 18:17:41 +0200

    First optimization pass. About 5x speedup.

--- img2prime.py
+++ img2prime.py
@@ -24,9 +24,35 @@ def linear_map(inputs, outputs):
     return mapping
 
 
-def find_next_prime(number, quiet=True):
+def probably_prime(
+    number,
+    primes=[
+        2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61,
+        67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137,
+        139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211,
+        223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283,
+        293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379,
+        383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461,
+        463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563,
+        569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643,
+        647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739,
+        743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829,
+        839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937,
+        941, 947, 953, 967, 971, 977, 983, 991, 997
+    ]
+):
+    """Check if a number is probably prime."""
+    for prime in primes:
+        "Rule out candidates with small prime factors first"
+        if number % prime == 0:
+            return False
+    "If the candidate might still be prime, use the more costly Miller-Rabin"
+    return miller_rabin(number)
+
+
+def find_next_prime(number, quiet=True, test_function=probably_prime):
     i = 1
-    while not miller_rabin(number):
+    while not test_function(number):
         if not quiet:
             print("checked {} candidates".format(i))
         i += 1
@@ -38,7 +64,8 @@ def find_prime_by_morphing(
         number,
         morphs,
         quiet=True,
-        printer=print
+        printer=print,
+        test_function=probably_prime
 ):
     trials = 0
 
@@ -57,7 +84,7 @@ def find_prime_by_morphing(
                     if not quiet:
                         printer('Tested {} numbers so far.'.format(trials))
                         printer(morphed)
-                    if miller_rabin(int(morphed)):
+                    if test_function(int(morphed)):
                         return int(morphed)
                     trials += 1
                     prime = find_prime_by_morphing_recursive(
